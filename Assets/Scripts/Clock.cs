0f748bb6f7aa86a73ccdebeeaf8cb14c666b2c98
ilacqua <ilacqua@OA-S141-11>
2025-01-30 11:49:56 -0500

    Create a clock that uses the loop mechanic of the sound to make a sound every 30 degrees / every hour

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
     public float hourSpeeds = 1f;
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
