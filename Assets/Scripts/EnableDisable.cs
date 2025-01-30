895d8a7e7f257b19fa6cf74c2c98c7288ff160c6
ilacqua <ilacqua@OA-S141-11>
2025-01-30 10:26:59 -0500

    Played around with enabling and disabling sprite renderer

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnableDisable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject go;
+    public SpriteRenderer sr;
+    public EnableDisable script;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            sr.enabled = false;
+            //script.enabled = false;
+            go.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            sr.enabled = true;
+            //script.enabled = true;
+            go.SetActive(true);
+        }
     }
 }
