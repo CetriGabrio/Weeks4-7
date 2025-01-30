d604d5f3189da6f7e63ef683a53b79344e0a0183
ilacqua <ilacqua@OA-S141-11>
2025-01-30 10:32:15 -0500

    Fixed a bug

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -13,14 +13,14 @@ public class EnableDisable : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            sr.enabled = false;
+            //sr.enabled = false;
             //script.enabled = false;
             go.SetActive(false);
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            sr.enabled = true;
+            //sr.enabled = true;
             //script.enabled = true;
             go.SetActive(true);
         }
