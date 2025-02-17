a6eb39ab93bb826f04780932fbc2bd84a0a9c4a3
Gabriele <gabriele.ilacqua@icloud.com>
2025-02-17 15:13:52 -0500

    Added the Pac Man effect so that the car teleports on the left border after crossing the right one

--- Assets/Scripts/ExplorableToy/CarMovement.cs
+++ Assets/Scripts/ExplorableToy/CarMovement.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     public float speed = 5f;
+    private float leftScreenEdge;
+    private float rightScreenEdge;
 
     void Start()
     {
+        leftScreenEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        rightScreenEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
     }
 
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (transform.position.x > rightScreenEdge)
+        {
+            transform.position = new Vector3(leftScreenEdge, transform.position.y, transform.position.z);
+        }
     }
 }
